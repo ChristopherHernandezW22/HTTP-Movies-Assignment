614e3ea23bdc537d88ca0db3ea26ae31f9c45c6b
Christopher Hernandez <cerberean661@gmail.com>
2019-09-18 19:54:39 -0700

    added inputs for title, director, metascore, stars

--- client/src/Movies/UpdateMovie.js
+++ client/src/Movies/UpdateMovie.js
@@ -1,9 +1,45 @@
 import React, { useState, useEffect } from 'react';
 
 const UpdateMovie = (props) => {
+    const [movie, setMovie] = useState({title: "",
+                                        director: "",
+                                        metascore: "",
+                                        stars: []
+                                        });
+    const handleChange = event => setMovie({...movie, 
+                                            [event.target.name]:
+                                            event.target.value});
+
+    const handleStars = index => event => {
+        setMovie({...movie, stars: movie.stars.map((star, starIndex) => {
+            return starIndex === index ? event.target.value: star;
+        })});
+    };
+
     return (
         <form>
-
+            <input type="text"
+                    name="title"
+                    placeholder="title"
+                    value={movie.title}
+                    onChange={handleChange} />
+            <input type="text"
+                    name="director"
+                    placeholder="director"
+                    value={movie.director}
+                    onChange={handleChange} />
+            <input type="text"
+                    name="metascore"
+                    placeholder="metascore"
+                    value={movie.metascore}
+                    onChange={handleChange} />
+            {movie.stars.map((actor, index) => {
+                return <input type="text"
+                                name="stars"
+                                placeholder="stars"
+                                value={actor}
+                                onChange={handleStars(index)} />
+            })}
         </form>
     );
 }
