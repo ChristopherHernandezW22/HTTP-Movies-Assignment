0f29211d9ccc2d53216022c64ba73e90eebfc15b
Christopher Hernandez <cerberean661@gmail.com>
2019-09-18 22:00:51 -0700

    updating movies work

--- client/src/Movies/UpdateMovie.js
+++ client/src/Movies/UpdateMovie.js
@@ -1,11 +1,21 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const UpdateMovie = (props) => {
-    const [movie, setMovie] = useState({title: "",
-                                        director: "",
-                                        metascore: "",
-                                        stars: []
-                                        });
+    console.log(props);
+    const [movie, setMovie] = useState(null);
+
+    const fetchMovie = id => {
+        axios
+            .get(`http://localhost:5000/api/movies/${id}`)
+            .then(res => setMovie(res.data))
+            .catch(err => console.log(err.response));
+    };
+
+    useEffect(() => {
+        fetchMovie(props.match.params.id)
+    }, [props.match.params.id]);
+
     const handleChange = event => setMovie({...movie, 
                                             [event.target.name]:
                                             event.target.value});
@@ -16,8 +26,22 @@ const UpdateMovie = (props) => {
         })});
     };
 
+    const handleSubmit = event => {
+        event.preventDefault();
+        axios.put(`http://localhost:5000/api/movies/${movie.id}`, movie)
+        .then(res => {
+            console.log(res);
+            props.history.push('/');
+        })
+        .catch(err => console.log(err.response));
+    };
+
+    if (!movie) {
+        return <div>Loading...</div>
+    }
+
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <input type="text"
                     name="title"
                     placeholder="title"
@@ -38,8 +62,10 @@ const UpdateMovie = (props) => {
                                 name="stars"
                                 placeholder="stars"
                                 value={actor}
+                                key={index}
                                 onChange={handleStars(index)} />
             })}
+            <button type="submit">Update</button>
         </form>
     );
 }
